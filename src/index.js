58580529bfc9e083322eac46c1dd8ab86a9c2a3d
ManoelMattos <manoelmattos@icloud.com>
2023-12-05 02:09:54 -0300

    Foram modificados os arquivos index.js e readme.md

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-qwsdefg
+class Character {
+    constructor(name, age, type) {
+      this.name = name;
+      this.age = age;
+      this.type = type;
+    }
+  
+    attack() {
+      let attack;
+      switch (this.type) {
+        case "mage":
+          attack = "magic";
+          break;
+        case "warrior":
+          attack = "sword";
+          break;
+        case "monk":
+          attack = "martial arts";
+          break;
+        case "ninja":
+          attack = "shuriken";
+          break;
+        default:
+          attack = "an unknown attack";
+      }
+  
+      console.log(`The ${this.type} attacked using ${attack}`);
+    }
+  }
+  
+  // Example of using the class
+  let mageCharacter = new Character("Merlin", 100, "mage");
+  let warriorCharacter = new Character("Arthur", 30, "warrior");
+  
+  mageCharacter.attack();          // Output: The mage attacked using magic
+  warriorCharacter.attack();       // Output: The warrior attacked using sword
+  
